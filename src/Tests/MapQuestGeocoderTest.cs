be1973508dd6b389ed4ff5c6ecefe764dccb9457
Huy Nguyen <huy@ronin.ws>
2014-05-09 15:12:46 -0700

    add MapQuest geocode test

---
+++ src/Tests/MapQuestGeocoderTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using Geocoding.MapQuest;
+
+namespace Geocoding.Tests
+{
+	public class MapQuestGeocoderTest : GeocoderTest
+	{
+		protected override IGeocoder CreateGeocoder()
+		{
+			string k = ConfigurationManager.AppSettings["mapQuestKey"];
+			return new MapQuestGeocoder(k);
+		}
+	}
+}
